bd8c51e8262dbab6747ab2ee8e0bbd8676113c60
Stéphane Sauvaget <s.sauvaget41000@gmail.com>
2021-06-14 23:20:29 +0200

    Create routing

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/programs", name="programs_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs", name="programs")
+     * @Route("/", name="name")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
         ]);
+    }
+    /**
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     */
+    public function show(int $id = 4): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => 4]);
     }
 }
